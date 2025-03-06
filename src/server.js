5ec4b9cf6aadcc23039cff796d6de105ae3bbf19
Aman <165676586+amancodeshere@users.noreply.github.com>
2025-03-07 00:12:35 +1100

    added route for inputOrder in server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+import { inputOrder } from "./databaseFunctions.js";
 
 const app = express();
 // Middleware to access the JSON body of requests
-app.use(express.json());
+app.use(bodyParser.json());
 // Middleware to allow access from other domains
 app.use(cors());
 // Middleware for logging errors
@@ -17,6 +20,26 @@ const HOST = process.env.IP || '127.0.0.1';
 // ============================= ROUTES BELOW ================================
 // ===========================================================================
 
+// Add order from ubl doc to database
+app.post('/api/orders/input', (req, res) => {
+    const {
+        SalesOrderId, UUID, IssueDate, PartyName, PayableAmount, PayableCurrencyCode,
+        ItemDescription, BuyersItemIdentification, SellersItemIdentification, ItemAmount, ItemUnitCode
+    } = req.body;
+
+    res.set('content-type', 'application/json');
+
+    inputOrder(
+        SalesOrderId, UUID, IssueDate, PartyName, PayableAmount, PayableCurrencyCode,
+        ItemDescription, BuyersItemIdentification, SellersItemIdentification, ItemAmount, ItemUnitCode,
+        (err, result) => {
+            if (err) {
+                return res.status(400).json({ error: err.message });
+            }
+            res.json(result);
+        }
+    );
+});
 
 // ===========================================================================
 // ============================= ROUTES ABOVE ================================
@@ -28,7 +51,7 @@ const server = app.listen(PORT, HOST, () => {
 
 process.on('SIGINT', () => {
     server.close(() => {
-      console.log('Shutting down server gracefully.');
-      process.exit();
+        console.log('Shutting down server gracefully.');
+        process.exit();
     });
 });
