75f12e2f83133ea4489661b522fa9bda38b336a8
Chloe McPhan <chloemcphan@outlook.com>
2025-03-15 13:47:14 +1100

    got schema-validator working for standard ubl invoices

--- src/validate.js
+++ src/validate.js
@@ -1,4 +1,12 @@
+import validator from 'xsd-schema-validator';
 
-function validate_invoice() {
-    
+async function validate_invoice() {
+    try {
+      const result = await validator.validateXML({ file: 'ubl/xml/UBL-Invoice-2.1-Example.xml' }, 'ubl/xsd/maindoc/UBL-Invoice-2.1.xsd');
+      console.log(result.valid);
+    } catch (err) {
+      console.error("validation error:");
+    }
 }
+
+validate_invoice();
