b16a43d3fc2c6bb6958783fe2b1b08d7c2dd46b4
Chloe McPhan <chloemcphan@outlook.com>
2025-03-15 16:03:23 +1100

    moved the schemas into a new folder, added ANZ PEPPOL schemas. Validate works for one of the PEPPOL schemas but throws a XPath parse error for the other

--- src/validate.js
+++ src/validate.js
@@ -1,10 +1,23 @@
-import validator from 'xsd-schema-validator';
+import xsdValidator from 'xsd-schema-validator';
+import validator from 'schematron-runner'
 
 async function validate_invoice() {
     try {
-      const result = await validator.validateXML({ file: 'ubl/xml/AU Invoice Energy Bill Example_1.xml' }, 'ubl/xsd/maindoc/UBL-Invoice-2.1.xsd');
+      const result = await xsdValidator.validateXML({ file: 'schemas/ubl/xml/AU Invoice Energy Bill Example_1.xml' }, 'schemas/ubl/xsd/maindoc/UBL-Invoice-2.1.xsd');
       console.log(result.valid);
+
+      // const other_result = await validator.validate('schemas/ubl/xml/AU Invoice Energy Bill Example_1.xml', 'schemas/ANZ-PEPPOL/AUNZ-UBL-validation.sch');
+      // conslole.log(other_result);
+
+      const results = await validator.validate('schemas/ubl/xml/AU Invoice Energy Bill Example_1.xml', 'schemas/ANZ-PEPPOL/AUNZ-PEPPOL-validation.sch');
+      console.log(results);
+
+      
+
     } catch (err) {
-      console.error("validation error");
+      console.error(err);
     }
 }
+
+
+validate_invoice();
