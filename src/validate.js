b21b348678788853af949263828cedd3af3d1660
Chloe McPhan <chloemcphan@outlook.com>
2025-03-15 14:23:19 +1100

    added AU-PEPPOL compliant invoice example

--- src/validate.js
+++ src/validate.js
@@ -2,11 +2,9 @@ import validator from 'xsd-schema-validator';
 
 async function validate_invoice() {
     try {
-      const result = await validator.validateXML({ file: 'ubl/xml/UBL-Invoice-2.1-Example.xml' }, 'ubl/xsd/maindoc/UBL-Invoice-2.1.xsd');
+      const result = await validator.validateXML({ file: 'ubl/xml/AU Invoice Energy Bill Example_1.xml' }, 'ubl/xsd/maindoc/UBL-Invoice-2.1.xsd');
       console.log(result.valid);
     } catch (err) {
-      console.error("validation error:");
+      console.error("validation error");
     }
 }
-
-validate_invoice();
