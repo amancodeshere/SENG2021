6ad3ae81b5581da9c7844a1b910d8006337ab744
Chloe McPhan <chloemcphan@outlook.com>
2025-03-16 23:40:48 +1100

    continued intergration test case

--- src/tests/intergration.test.js
+++ src/tests/intergration.test.js
@@ -15,23 +15,26 @@ describe('Intergration tests for all routes', () => {
         email: VALID_EMAIL,
         password: VALID_PASSWORD
     };
-    const validXMLDocument = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <order>
-            <SalesOrderID>12345678</SalesOrderID>
-            <IssueDate>2025-03-06</IssueDate>
-            <PartyName>ABC Corp</PartyName>
-            <PayableAmount>500</PayableAmount>
-            <CurrencyCode>USD</CurrencyCode>
-        </order>
-    `;
-    const validJSONDocument = {
-        SalesOrderID: "12345678",
-        IssueDate: "2025-03-06",
-        PartyName: "ABC Corp",
-        PayableAmount: 500,
-        CurrencyCode: "USD"
-    };
+    const validXMLDocument = `<?xml version="1.0" encoding="UTF-8"?>
+<Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"
+       xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+       xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
+  <cbc:ID>12345678</cbc:ID>
+  <cbc:IssueDate>2025-03-06</cbc:IssueDate>
+  <cac:AccountingCustomerParty>
+    <cac:Party>
+      <cac:PartyName>
+        <cbc:Name>ABC Corp</cbc:Name>
+      </cac:PartyName>
+    </cac:Party>
+  </cac:AccountingCustomerParty>
+  <cac:LegalMonetaryTotal>
+    <cbc:PayableAmount currencyID="USD">500</cbc:PayableAmount>
+  </cac:LegalMonetaryTotal>
+  <cac:OrderReference>
+    <cbc:ID>12345678</cbc:ID>
+  </cac:OrderReference>
+</Invoice>`;
 
 
     test("Single user successfully creates and exports an XML invoice", async () => {
@@ -55,6 +58,12 @@ describe('Intergration tests for all routes', () => {
         expect(response.status).toBe(200);
 
         var invoiceId = res.body.invoiceId;
+
+        res = await request(app)
+            .get(`/api/v1/invoice/${invoiceId}/xml`)
+            .set('sessionid', sessionId);
+
+        
 
         
 
