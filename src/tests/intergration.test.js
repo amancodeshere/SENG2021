a5030b949c4b2a7ea61c8f401644de79bc310cef
Chloe McPhan <chloemcphan@outlook.com>
2025-03-16 20:18:22 +1100

    continued intergration tests

--- src/tests/intergration.test.js
+++ src/tests/intergration.test.js
@@ -1,7 +1,6 @@
 import request from 'supertest';
-import { app } from '../../app.js';
-import { userInput } from '../../UsersToDB.js';
-import { CustomInputError } from '../../errors.js';
+import { app } from '../app.js';
+import expect from 'expect';
 
 // constants for request parameters
 const VALID_EMAIL = 'valid@gmail.com';
@@ -9,18 +8,6 @@ const VALID_COMPANY_NAME = 'Valid Pty Ltd';
 const VALID_PASSWORD = 'Password123';
 const SECOND_EMAIL = 'candle.craft@gmail.com'
 const SECOND_COMPANY_NAME = 'Candlecraft Pty Ltd';
-const USERID1 = 1;
-const USERID2 = 2;
-const SESSIONID1 = 123;
-const SESSIONID2 = 456;
-
-jest.mock('../../UsersToDB.js', () => ({
-    userInput: jest.fn()
-}));
-
-beforeEach(() => {
-    jest.clearAllMocks();
-});
 
 describe('Intergration tests for all routes', () => {
     const user = {
@@ -28,21 +15,48 @@ describe('Intergration tests for all routes', () => {
         email: VALID_EMAIL,
         password: VALID_PASSWORD
     };
+    const validXMLDocument = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <order>
+            <SalesOrderID>12345678</SalesOrderID>
+            <IssueDate>2025-03-06</IssueDate>
+            <PartyName>ABC Corp</PartyName>
+            <PayableAmount>500</PayableAmount>
+            <CurrencyCode>USD</CurrencyCode>
+        </order>
+    `;
+    const validJSONDocument = {
+        SalesOrderID: "12345678",
+        IssueDate: "2025-03-06",
+        PartyName: "ABC Corp",
+        PayableAmount: 500,
+        CurrencyCode: "USD"
+    };
+
+
     test("Single user successfully creates and exports an XML invoice", async () => {
-        userInput.mockImplementationOnce((email, password, company, callback) => {
-            callback(null, {
-                success: true,
-                message: "User registered.",
-                userID: USERID1,
-                sessionID: SESSIONID1
-            });
-        });
-        const res = await request(app)
+        var res = await request(app)
             .post('/api/v1/admin/register')
             .set('Content-Type', 'application/json')
             .send(user);
-        expect(res.body).toEqual({ sessionId: SESSIONID1 });
+        
+        expect(res.body).toEqual({ sessionId:  expect.any(Number)});
         expect(res.status).toBe(200);
+
+        var sessionId = res.body.sessionId;
+
+        res = await request(app)
+            .post('/api/v1/invoice')
+            .set('sessionId', sessionId)
+            .set('Content-Type', 'application/xml')
+            .send(validXMLDocument);
+        
+        expect(res.body).toEqual({ invoiceId: expect.any(Number) });
+        expect(response.status).toBe(200);
+
+        var invoiceId = res.body.invoiceId;
+
+        
 
     });
 });
