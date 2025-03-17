e778765ff8365d2e1e1f77dc0f81999b145cc06f
Ayra Saqib <z5534128@ad.unsw.edu.au>
2025-03-17 11:23:16 +1100

    edited the route for list invoices so that sessionid is taken from the header and new listInvoices function is used

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import {
     adminRegister,
     adminLogin
 } from './admin.js';
-import {invoiceToXml, viewInvoice, validateInvoice, getInvoicesBySession} from './invoice.js';
+import { invoiceToXml, viewInvoice, validateInvoice, listInvoices } from './invoice.js';
 import { getUserBySessionId } from "./UsersToDB.js";
-import { handlePostInvoice } from './postInvoice.js';
+import { handlePostInvoice } from './invoice.js';
 import { healthCheck } from './health.js';
 
 export const app = express();
@@ -20,7 +20,6 @@ app.use((req, res, next) => {
         bodyParser.json()(req, res, next);
     }
 });
-
 // Middleware to access the JSON body of requests
 app.use(bodyParser.urlencoded({ extended: true }));
 // Middleware to allow access from other domains
@@ -34,19 +33,6 @@ app.use(morgan('dev'));
 
 // Health check route
 app.get('/api/health', healthCheck);
-
-// Get an invoice list given suer session
-app.get('/api/invoices/list/:sessionId', (req, res) => {
-    const sessionId = parseInt(req.params.sessionId, 10);
-
-    getInvoicesBySession(sessionId, (err, result) => {
-        res.set("Content-Type", "application/json");
-        if (err) {
-            return res.status(400).json({ error: err.message });
-        }
-        res.status(200).json(result);
-    });
-});
 
 // register a new user
 app.post('/api/v1/admin/register', bodyParser.json(), (req, res) => {
@@ -72,6 +58,11 @@ app.post('/api/v1/admin/login', bodyParser.json(), (req, res) => {
 
         res.status(200).json(result);
     });
+});
+
+// Create new invoice
+app.post('/api/invoice', (req, res) => {
+    handlePostInvoice(req, res);
 });
 
 // view an invoice
@@ -125,9 +116,23 @@ app.post('/api/v1/invoice/validate', bodyParser.json(), (req, res) => {
 
 });
 
-// Create new invoice
-app.post('/api/invoice', (req, res) => {
-    handlePostInvoice(req, res);
+// View an list of sessions by partyNameBuyer
+app.get('/api/v1/invoices/list', (req, res) => {
+    const sessionId = parseInt(req.headers.sessionid);
+    const partyNameBuyer = req.query.partyNameBuyer
+
+    getUserBySessionId(sessionId, (sessionErr, user) => {
+        if (sessionErr) {
+            return res.status(401).json({ error: sessionErr.message });
+        }
+
+        listInvoices(partyNameBuyer, (err, result) => {
+            if (err) {
+                return res.status(400).json({ error: err.message });
+            }
+            res.status(200).json(result);
+        });
+    });
 });
 
 // ===========================================================================
