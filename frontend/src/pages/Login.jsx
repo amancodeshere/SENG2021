efc088071d0ffec71f01b222fcffcd707ef7e090
Ayra Saqib <z5534128@ad.unsw.edu.au>
2025-04-05 23:20:25 +1100

    created the Login page in Login.jsx which includes a form for email and password submission and a request to login route in our api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,60 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import "../css/Login.css"
+
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const res = await axios.post("/api/v1/admin/login", {
+              email,
+              password,
+            })
+            localStorage.setItem("token", res.data.sessionId)
+            navigate("/dashboard")
+        } catch (err) {
+            setError(err.response.data.error || "Login failed.")
+        }
+    }
+    
     return (
-    <>
-    </>
+        <div className="login-container">
+            <img className="login-logo" src="/logo1.png" alt="TradeDocs Navigator Logo" />
+            <h2 className="login-heading">Login</h2>
+            <form onSubmit={handleSubmit} className="login-form">
+                <p className="input-label">Email</p>
+                <input
+                    type="text"
+                    placeholder="Enter email"
+                    className="email-input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <p className="input-label">Password</p>
+                <input
+                    type="password"
+                    placeholder="Enter password"
+                    className="password-input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className="login-button">
+                    Login
+                </button>
+            </form>
+            {error && <p className="login-error">{error}</p>}
+            <p className="register-text">Don't have an account?</p>
+            <Link to="/register" className="register-link">
+                Register
+            </Link>
+        </div>
     )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://seng2021-production-e448.up.railway.app',
+        changeOrigin: true,
+      },
+    },
+  },
 })
