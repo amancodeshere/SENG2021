6012b477542d2dc2b41c3f5cb9e779ca0b2cd656
Ayra Saqib <z5534128@ad.unsw.edu.au>
2025-04-06 00:09:54 +1100

    changed dahsboard page to be a private page only accessible if there is a token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import PrivateRoute from "./PrivateRoute"
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
         <Route path="/" element={<Landing />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </main>
   )
