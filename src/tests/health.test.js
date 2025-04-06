1ce48d7eb01f874d0c711d14a818862deab3306c
Aman <165676586+amancodeshere@users.noreply.github.com>
2025-04-07 01:39:26 +1000

    fixed heath test for new postgresDB

--- src/tests/health.test.js
+++ src/tests/health.test.js
@@ -2,23 +2,19 @@ import { healthCheck } from '../health.js';
 import { db } from '../connect.js';
 import expect from "expect";
 
-
 jest.mock('../connect.js', () => ({
     db: {
         query: jest.fn(),
     },
 }));
 
-
 describe("healthCheck Function", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-
-    // TODO: ** Fix this test before merging into main **
     test("success should be returned if database is connected", async () => {
-        db.query.mockResolvedValueOnce({}); // simulate a successful query
+        db.query.mockResolvedValueOnce({});
 
 
         const mockRes = {
@@ -26,34 +22,28 @@ describe("healthCheck Function", () => {
             json: jest.fn(),
         };
 
-
         await healthCheck({}, mockRes);
 
-
         expect(mockRes.status).toHaveBeenCalledWith(200);
-        expect(mockRes.json).toHaveBeenCalledWith(
-            expect.any({
-                database: "connected",
-                memoryUsage: expect.any(String),
-                status: "success",
-                Uptime: expect.any(String),
-            })
-        );
+
+        expect(mockRes.json).toHaveBeenCalledWith(expect.objectContaining({
+            database: "connected",
+            status: "success",
+            memoryUsage: expect.any(String),
+            uptime: expect.any(String),
+        }));
     });
 
 
     test("fail should be returned if database is not connected", async () => {
         db.query.mockRejectedValueOnce(new Error("Database connection failed"));
 
-
         const mockRes = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         };
 
-
         await healthCheck({}, mockRes);
-
 
         expect(mockRes.status).toHaveBeenCalledWith(500);
         expect(mockRes.json).toHaveBeenCalledWith({
@@ -62,7 +52,5 @@ describe("healthCheck Function", () => {
         });
     });
 });
-
-
 
 
