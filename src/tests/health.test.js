6c91e41f02598db734a133f9e1842c57de59e4c3
Aman <165676586+amancodeshere@users.noreply.github.com>
2025-04-07 01:29:12 +1000

    commented out integration tests

--- src/tests/health.test.js
+++ src/tests/health.test.js
@@ -2,27 +2,33 @@ import { healthCheck } from '../health.js';
 import { db } from '../connect.js';
 import expect from "expect";
 
+
 jest.mock('../connect.js', () => ({
     db: {
         query: jest.fn(),
     },
 }));
 
+
 describe("healthCheck Function", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
+
     // TODO: ** Fix this test before merging into main **
     test("success should be returned if database is connected", async () => {
         db.query.mockResolvedValueOnce({}); // simulate a successful query
+
 
         const mockRes = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         };
 
+
         await healthCheck({}, mockRes);
+
 
         expect(mockRes.status).toHaveBeenCalledWith(200);
         expect(mockRes.json).toHaveBeenCalledWith(
@@ -35,15 +41,19 @@ describe("healthCheck Function", () => {
         );
     });
 
+
     test("fail should be returned if database is not connected", async () => {
         db.query.mockRejectedValueOnce(new Error("Database connection failed"));
+
 
         const mockRes = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         };
 
+
         await healthCheck({}, mockRes);
+
 
         expect(mockRes.status).toHaveBeenCalledWith(500);
         expect(mockRes.json).toHaveBeenCalledWith({
@@ -52,3 +62,7 @@ describe("healthCheck Function", () => {
         });
     });
 });
+
+
+
+
