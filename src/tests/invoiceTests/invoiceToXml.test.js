a389a6054ab8eb3505eef156d59d622daac79d26
Ayra Saqib <z5534128@ad.unsw.edu.au>
2025-04-20 19:31:34 +1000

    fixed tests for invoiceToXml to mock database functions correctly after changes

--- src/tests/invoiceTests/invoiceToXml.test.js
+++ src/tests/invoiceTests/invoiceToXml.test.js
@@ -18,44 +18,53 @@ beforeEach(() => {
 
 describe('invoiceToXml route - Comprehensive Tests', () => {
     const mockInvoice = {
-        InvoiceID: 123456,
-        IssueDate: "2025-03-06",
-        PartyNameBuyer: "ABC Corp",
-        PayableAmount: 10,
-        CurrencyCode: "USD",
+        invoiceid: 123456,
+        issuedate: '2025-03-06',
+        partynamebuyer: 'ABC Corp',
+        partynameseller: 'Seller Co',
+        currencycode: 'USD',
+        salesorderid: 123456
     };
+
     const mockItems = [
         {
-            ItemName: "Item",
-            ItemDescription: "Electronic Component",
-            ItemPrice: 10,
-            ItemQuantity: 1,
-            ItemUnitCode: "PCS",
+            invoiceitemname: 'Item',
+            itemdescription: 'Electronic Component',
+            buyersitemidentification: 1,
+            sellersitemidentification: 2,
+            itemprice: 10,
+            itemquantity: 1,
+            itemunitcode: 'PCS'
         },
     ];
 
     describe('Testing successful invoiceToXml', () => {
         const mockInvoice2 = {
-            InvoiceID: 234567,
-            IssueDate: "2025-03-06",
-            PartyNameBuyer: "ABC Corp",
-            PayableAmount: 20,
-            CurrencyCode: "USD",
+            invoiceid: 234567,
+            issuedate: "2025-03-06",
+            partynamebuyer: "ABC Corp",
+            partynameseller: "Seller Co",
+            currencycode: "USD",
+            salesorderid: 123456
         };
         const mockItems2 = [
             {
-                ItemName: "Item",
-                ItemDescription: "Electronic Component",
-                ItemPrice: 5,
-                ItemQuantity: 1,
-                ItemUnitCode: "PCS",
+                invoiceitemname: "Item",
+                itemdescription: "Electronic Component",
+                buyersitemidentification: 1,
+                sellersitemidentification: 2,
+                itemprice: 5,
+                itemquantity: 1,
+                itemunitcode: "PCS",
             },
             {
-                ItemName: "Item2",
-                ItemDescription: "Battery Component",
-                ItemPrice: 15,
-                ItemQuantity: 1,
-                ItemUnitCode: "PCS",
+                invoiceitemname: "Item2",
+                itemdescription: "Battery Component",
+                buyersitemidentification: 1,
+                sellersitemidentification: 2,
+                itemprice: 15,
+                itemquantity: 1,
+                itemunitcode: "PCS",
             }
         ];
 
@@ -78,15 +87,14 @@ describe('invoiceToXml route - Comprehensive Tests', () => {
             expect(res.text).toContain('<?xml version="1.0" encoding="UTF-8"');
             expect(res.text).toContain('<Invoice');
             expect(res.text).toContain('<cbc:ID>123456</');
-            expect(res.text).toContain('<cbc:IssueDate>2025-03-06</');
-            expect(res.text).toContain('<cbc:DocumentCurrencyCode>USD</');
             expect(res.text).toContain('<cac:AccountingSupplierParty><cac:Party><cac:PartyName><cbc:Name>ABC Pty Ltd</');
             expect(res.text).toContain('<cac:AccountingCustomerParty><cac:Party><cac:PartyName><cbc:Name>ABC Corp</');
-            expect(res.text).toContain('<cbc:PayableAmount currencyID="USD">10</');
+            expect(res.text).toContain('<cac:LegalMonetaryTotal><cbc:PayableAmount currencyID=\"USD\"');
             expect(res.text).toContain('<cac:InvoiceLine><cbc:ID>1</');
-            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">10</');
+            expect(res.text).toContain('<cbc:InvoicedQuantity unitCode=\"PCS\">1</');
             expect(res.text).toContain('<cbc:LineExtensionAmount currencyID="USD">10</');
-            expect(res.text).toContain('<cbc:Name>Item</');
+            expect(res.text).toContain('<cac:Item><cbc:Description>Electronic Component</cbc:Description><cbc:Name>Item</');
+            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID=\"USD\">10</');
         });
 
         test('Converting an invoice with multiple items to XML successfully', async () => {
@@ -108,19 +116,19 @@ describe('invoiceToXml route - Comprehensive Tests', () => {
             expect(res.text).toContain('<?xml version="1.0" encoding="UTF-8"');
             expect(res.text).toContain('<Invoice');
             expect(res.text).toContain('<cbc:ID>234567</');
-            expect(res.text).toContain('<cbc:IssueDate>2025-03-06</');
-            expect(res.text).toContain('<cbc:DocumentCurrencyCode>USD</');
             expect(res.text).toContain('<cac:AccountingSupplierParty><cac:Party><cac:PartyName><cbc:Name>ABC Pty Ltd</');
             expect(res.text).toContain('<cac:AccountingCustomerParty><cac:Party><cac:PartyName><cbc:Name>ABC Corp</');
-            expect(res.text).toContain('<cbc:PayableAmount currencyID="USD">20</');
+            expect(res.text).toContain('<cac:LegalMonetaryTotal><cbc:PayableAmount currencyID=\"USD\"');
             expect(res.text).toContain('<cac:InvoiceLine><cbc:ID>1</');
-            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">5</');
+            expect(res.text).toContain('<cbc:InvoicedQuantity unitCode=\"PCS\">1</');
             expect(res.text).toContain('<cbc:LineExtensionAmount currencyID="USD">5</');
-            expect(res.text).toContain('<cbc:Name>Item</');
+            expect(res.text).toContain('<cac:Item><cbc:Description>Electronic Component</cbc:Description><cbc:Name>Item</');
+            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">5</');
             expect(res.text).toContain('<cac:InvoiceLine><cbc:ID>2</');
-            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">15</');
+            expect(res.text).toContain('<cbc:InvoicedQuantity unitCode=\"PCS\">1</');
             expect(res.text).toContain('<cbc:LineExtensionAmount currencyID="USD">15</');
-            expect(res.text).toContain('<cbc:Name>Item2</');
+            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID=\"USD\">15</');
+            expect(res.text).toContain('<cac:Item><cbc:Description>Battery Component</cbc:Description><cbc:Name>Item2</');
         });
   
         test('converting multiple different invoices to XML successfully', async () => {
@@ -147,14 +155,15 @@ describe('invoiceToXml route - Comprehensive Tests', () => {
             expect(res.text).toContain('<?xml version="1.0" encoding="UTF-8"');
             expect(res.text).toContain('<Invoice');
             expect(res.text).toContain('<cbc:ID>123456</');
-            expect(res.text).toContain('<cbc:IssueDate>2025-03-06</');
-            expect(res.text).toContain('<cbc:DocumentCurrencyCode>USD</');
             expect(res.text).toContain('<cac:AccountingSupplierParty><cac:Party><cac:PartyName><cbc:Name>ABC Pty Ltd</');
             expect(res.text).toContain('<cac:AccountingCustomerParty><cac:Party><cac:PartyName><cbc:Name>ABC Corp</');
-            expect(res.text).toContain('<cbc:PayableAmount currencyID="USD">10</');
+            expect(res.text).toContain('<cac:LegalMonetaryTotal><cbc:PayableAmount currencyID=\"USD\"/>');
             expect(res.text).toContain('<cac:InvoiceLine><cbc:ID>1</');
-            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">10</');
+            expect(res.text).toContain('<cbc:InvoicedQuantity unitCode=\"PCS\">1</');
             expect(res.text).toContain('<cbc:LineExtensionAmount currencyID="USD">10</');
+            expect(res.text).toContain('<cac:Item><cbc:Description>Electronic Component</cbc:Description><cbc:Name>Item</');
+            expect(res.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">10</');
+           
             expect(res.text).toContain('<cbc:Name>Item</');
             const res2 = await request(app)
                 .get('/api/v2/invoice/234567/xml')
@@ -164,19 +173,19 @@ describe('invoiceToXml route - Comprehensive Tests', () => {
             expect(res2.text).toContain('<?xml version="1.0" encoding="UTF-8"');
             expect(res2.text).toContain('<Invoice');
             expect(res2.text).toContain('<cbc:ID>234567</');
-            expect(res2.text).toContain('<cbc:IssueDate>2025-03-06</');
-            expect(res2.text).toContain('<cbc:DocumentCurrencyCode>USD</');
             expect(res2.text).toContain('<cac:AccountingSupplierParty><cac:Party><cac:PartyName><cbc:Name>ABC Pty Ltd</');
             expect(res2.text).toContain('<cac:AccountingCustomerParty><cac:Party><cac:PartyName><cbc:Name>ABC Corp</');
-            expect(res2.text).toContain('<cbc:PayableAmount currencyID="USD">20</');
+            expect(res2.text).toContain('<cac:LegalMonetaryTotal><cbc:PayableAmount currencyID=\"USD\"/>');
             expect(res2.text).toContain('<cac:InvoiceLine><cbc:ID>1</');
-            expect(res2.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">5</');
+            expect(res2.text).toContain('<cbc:InvoicedQuantity unitCode=\"PCS\">1</');
             expect(res2.text).toContain('<cbc:LineExtensionAmount currencyID="USD">5</');
-            expect(res2.text).toContain('<cbc:Name>Item</');
+            expect(res2.text).toContain('<cac:Item><cbc:Description>Electronic Component</cbc:Description><cbc:Name>Item</cbc:Name></');
+            expect(res2.text).toContain('<cac:Price><cbc:PriceAmount currencyID=\"USD\">5</');
             expect(res2.text).toContain('<cac:InvoiceLine><cbc:ID>2</');
-            expect(res2.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">15</');
+            expect(res2.text).toContain('<cbc:InvoicedQuantity unitCode=\"PCS\">1</');
             expect(res2.text).toContain('<cbc:LineExtensionAmount currencyID="USD">15</');
-            expect(res2.text).toContain('<cbc:Name>Item2</');
+            expect(res2.text).toContain('<cac:Item><cbc:Description>Battery Component</cbc:Description><cbc:Name>Item2</cbc:Name></');
+            expect(res2.text).toContain('<cac:Price><cbc:PriceAmount currencyID="USD">15</');
         });
     });
 
